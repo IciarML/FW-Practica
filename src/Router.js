1932e80acaa7156dc3c94a26770a49663d6f23a0
chubi0l <o.chubinova.2022@alumnos.urjc.es>
2023-12-08 19:32:22 +0100

    Trying to validate the forms values

--- src/Router.js
+++ src/Router.js
@@ -14,6 +14,54 @@ router.post('/new', (req, res) => {
     let { titulo, autor, genero, sinopsis, isbn, precio, editorial, idioma, imagen, optionRadios } = req.body;
     //Se llama a una función (addPost) para agregar una nueva publicación
     Service.addPost({ titulo, autor, genero, sinopsis, isbn, precio, editorial, idioma, imagen, optionRadios });
+
+    form.addEventListener("submit", e =>{
+        let warnings = ""
+        let entrar = false
+        if(titulo.value.length < 1){
+            warnings += 'Debes poner un titulo'
+            entrar = true
+        }
+        if(autor.value.length < 1){
+            warnings += 'Debes poner un autor'
+            entrar = true
+        }
+        if(genero.value = "..."){
+            warnings += 'Debes poner un genero'
+            entrar = true
+        }
+        if(sinopsis.value.length < 1){
+            warnings += 'Debes poner un sinopsis'
+            entrar = true
+        }
+        if(isbn.value.length < 1){
+            warnings += 'Debes poner un isbn'
+            entrar = true
+        }
+        if(precio.value.length < 1){
+            warnings += 'Debes poner un precio'
+            entrar = true
+        }
+        if(editorial.value.length < 1){
+            warnings += 'Debes poner un editorial'
+            entrar = true
+        }
+        if(idioma.value = "..."){
+            warnings += 'Debes poner un idioma'
+            entrar = true
+        }
+        if(imagen.value.length < 1){
+            warnings += 'Debes poner un imagen'
+            entrar = true
+        }
+        if(entrar) {
+            parrafo.innerHTML = warnings
+        }
+        else {
+            parrafo.innerHTML = "Enviado"
+        }
+        
+    })
     //Se renderiza a savedBook
     res.render('savedBook');
 });
