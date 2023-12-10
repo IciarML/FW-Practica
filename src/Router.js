897f433e1cf2e99e132fbd2203d9fe09c20a22e0
chubi0l <o.chubinova.2022@alumnos.urjc.es>
2023-12-10 01:02:11 +0100

    A lot of updates (forms, css, router, validation...)

--- src/Router.js
+++ src/Router.js
@@ -15,37 +15,54 @@ router.post('/new', (req, res) => {
     let { titulo, autor, genero, sinopsis, isbn, precio, editorial, idioma, imagen, optionRadios } = req.body;
     let notCorrectTitulo = ""
     let notCorrectAutor = ""
-    let notCorrectGenero = "..."
-    /*let notCorrectSinopsis = ""
-    let notCorrectIsbn = ""
     let notCorrectPrecio = ""
-    let notCorrectEditorial = ""
-    let notCorrectIdioma = ""
-    let notCorrectImagen = ""
-    let notCorrectOptionRadios = ""*/
     //Se llama a una función (addPost) para agregar una nueva publicación
-    Service.addPost({ titulo, autor, genero, sinopsis, isbn, precio, editorial, idioma, imagen, optionRadios });
-
-    //Se renderiza a savedBook
-    res.render('savedBook',  {
+    if(req.body.titulo == '' || req.body.autor == '' || req.body.precio == ''){
+        //Se renderiza a savedBook
+        res.render('savedBook',  {
         posts: Service.getPosts(),
-    success: req.body.titulo != notCorrectTitulo,
-    success2: req.body.autor != notCorrectAutor,
-    success3: req.body.genero != notCorrectGenero,
-    bien: (req.body.titulo != notCorrectTitulo) && (req.body.autor != notCorrectAutor) && (req.body.genero != notCorrectGenero),
+        success: req.body.titulo != notCorrectTitulo,
+        success2: req.body.autor != notCorrectAutor,
+        success3: req.body.precio != notCorrectPrecio,
+        bien: (req.body.titulo != notCorrectTitulo) && (req.body.autor != notCorrectAutor) && (req.body.precio != notCorrectPrecio),
 });
+    }
+    else{
+        res.render('savedBook',  {
+            posts: Service.getPosts(),
+            success: req.body.titulo != notCorrectTitulo,
+            success2: req.body.autor != notCorrectAutor,
+            success3: req.body.precio != notCorrectPrecio,
+            bien: (req.body.titulo != notCorrectTitulo) && (req.body.autor != notCorrectAutor) && (req.body.precio != notCorrectPrecio),
+        });
+        Service.addPost({ titulo, autor, genero, sinopsis, isbn, precio, editorial, idioma, imagen, optionRadios });
+    }; 
 });
 
 
-
+//Ruta de la pagina detalle
 router.get('/post/:id', (req, res) => {
     let post = Service.getPost(req.params.id);
     res.render('productDescription', { post });
 });
 
+//Ruta de la pagina intermedia al borrar
 router.get('/post/:id/delete', (req, res) => {
     Service.deletePost(req.params.id);
     res.render('deleteBook');
 });
 
+
+//Rutas del formulario
+router.get('/formulario', (req, res) => {
+    res.render('Formulario');
+ });
+ 
+//Modificar plato
+router.get('/post/:id/modify', (req, res) => { 
+    let post = Service.getPost(req.params.id);  
+    res.render('Formulario', {post});
+ }); 
+
+ 
 export default router;
--- src/Service.js
+++ src/Service.js
@@ -1,9 +1,9 @@
 const posts = new Map();
 let nextId = 0;
 
-addPost({imagen: "public/Photos/Image1.jpg", titulo: "HARRY POTTER COLECCIÓN COMPLETA", autor: "J.K. Rowling", precio: "80.68",
+addPost({imagen: "Photos/Image1.jpg", titulo: "HARRY POTTER COLECCIÓN COMPLETA", autor: "J.K. Rowling", precio: "80.68",
         genero: "Fantasía", sinopsis: "...", isbn: "9788408224228", editorial:"...", idioma: "Castellano", optionRadios: "Tapa blanda"}); 
-addPost({imagen: "public/Photos/Image2.jpg", titulo: "THE BELL JAR", autor: "Sylvia Plath", precio: "11.18",
+addPost({imagen: "Photos/Image2.jpg", titulo: "THE BELL JAR", autor: "Sylvia Plath", precio: "11.18",
         genero: "Biografía", sinopsis: "...", isbn: "9788408224228", editorial:"...", idioma: "Inglés", optionRadios: "Tapa blanda"}); 
 addPost({imagen: "public/Photos/Image3.jpg", titulo: "RETORNO DE LAS ESTRELLAS", autor: "Stanisław Lem", precio: "13.77",
         genero: "Ficción", sinopsis: "...", isbn: "9788408224228", editorial:"...", idioma: "Castellano", optionRadios: "Tapa blanda"}); 
@@ -21,11 +21,11 @@ addPost({imagen: "public/Photos/Image8.jpg", titulo: "LA BIBLIOTECA DE LA MEDIA 
         genero: "Ficción", sinopsis: "...", isbn: "9788408224228", editorial:"...", idioma: "Castellano", optionRadios: "Tapa blanda"}); 
 addPost({imagen: "public/Photos/Image9.jpg", titulo: "MONET: THE TRIUMPH OF IMPRESSIONISM", autor: "Daniel Wildenstein", precio: "15.20",
         genero: "Historia", sinopsis: "...", isbn: "9788408224228", editorial:"...", idioma: "Inglés", optionRadios: "Tapa blanda"}); 
-addPost({imagen: "public/Photos/Image10.jpg", titulo: "EIGHTEEN", autor: "Alberto Ramos", precio: "12.25",
+addPost({imagen: "../Photos/Image10.jpg", titulo: "EIGHTEEN", autor: "Alberto Ramos", precio: "12.25",
         genero: "Poesía", sinopsis: "...", isbn: "9788408224228", editorial:"...", idioma: "Castellano", optionRadios: "Tapa blanda"}); 
-addPost({imagen: "public/Photos/Image11.jpg", titulo: "VEINTE MIL LEGUAS DE VIAJE SUBMARINO", autor: "Julio Verne", precio: "14.96",
+addPost({imagen: "../Photos/Image11.jpg", titulo: "VEINTE MIL LEGUAS DE VIAJE SUBMARINO", autor: "Julio Verne", precio: "14.96",
         genero: "Aventura", sinopsis: "...", isbn: "9788408224228", editorial:"...", idioma: "Castellano", optionRadios: "Tapa blanda"}); 
-addPost({imagen: "public/Photos/Image12.jpg", titulo: "LOS PACIENTES DEL DOCTOR GARCÍA", autor: "Almudena Grandes", precio: "21.75",
+addPost({imagen: "../Photos/Image12.jpg", titulo: "LOS PACIENTES DEL DOCTOR GARCÍA", autor: "Almudena Grandes", precio: "21.75",
         genero: "Historia", sinopsis: "...", isbn: "9788408224228", editorial:"...", idioma: "Castellano", optionRadios: "Tapa blanda"}); 
 
 
@@ -46,4 +46,15 @@ export function getPosts(){
 export function getPost(id){
     return posts.get(id);
 }
+
+/*export function modificarLibro(){
+    document.querySelector('forms')
+    .addEventListener('submit', e=> {
+      e.preventDefault()
+      const data = Object.fromEntries(
+        new FormData(e.target)
+      )
+      alert(JSON.stringify(data))
+    })
+}*/
 
